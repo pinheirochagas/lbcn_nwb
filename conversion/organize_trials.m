d013a83e989fb3cdfb70d970f99ea1ddc111dc44
Areti Majumdar <areti@MacBook-Pro.local>
2021-05-21 19:23:28 -0700

    organize trials function

---
+++ conversion/organize_trials.m
@@ -0,0 +1,10 @@
+function [organized_trials] = organize_trials(sbj_name)
+
+
+%goes through all blocks, creates 'trial' time interval object for each block
+%and adds to an array, array will go in intervals_trials group in nwb file
+block_array = types.core.TimeIntervals.empty;
+
+
+end
+
